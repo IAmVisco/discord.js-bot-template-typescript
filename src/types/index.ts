acfa368508c0a99abeb45c9bbe576657d2b4114f
Alexander <sashok935@gmail.com>
2020-07-14 00:49:50 +0300

    Add tsconfig and types

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+import type {
+  Client, Collection, Message, PermissionResolvable, Snowflake,
+} from 'discord.js';
+
+export interface CustomClient extends Client {
+  commands?: Collection<string, Command>;
+}
+
+export interface CustomMessage extends Message {
+  client: CustomClient
+}
+
+export interface Command {
+  name: string;
+  group: string;
+  args?: boolean;
+  usage?: string;
+  aliases?: Array<string>;
+  description: string;
+  hidden?: boolean;
+  permissions?: PermissionResolvable;
+  role?: Snowflake;
+  execute: (message: CustomMessage, args?: Array<string>) => Promise<CustomMessage | void>;
+}
